1bd17f287338b0710dc39a9e84caddc5700d4324
Dewansh <dewanshparashar@gmail.com>
2022-06-22 12:51:58 +0530

    Dev : Updated OG image name to maybe see if twitter card image works

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Dewansh Parashar is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://dewansh.vercel.app', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/og_1.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@dewanshparashar',
   },
   plugins: [
