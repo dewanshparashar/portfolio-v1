4de7d6f64118303a5d8dfa75a6fd7224fda0d91b
Dewansh <dewanshparashar@yahoo.in>
2022-06-22 12:35:27 +0530

    Dev : Updated the site url in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Dewansh Parashar',
     description:
       'Dewansh Parashar is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://dewansh.vercel.app', // No trailing slash allowed!
+    siteUrl: 'https://dewansh.xyz', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@dewanshparashar',
   },
